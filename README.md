93ebdd17ccb3e66bb1a1fef6e699327532ceb0e5
Asaf David <adavid@pingidentity.compingidentity.com>
2017-10-21 19:59:47 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# pingid
+PingID python package for easy access to pingid API
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pingid',
+    version='1.0',
+    packages=['pingid'],
+    url='https://github.com/pingidentity/pingid-python-package/',
+    license='Apache 2.0',
+    author='Asaf David',
+    author_email='adavid@pingidentity.com',
+    description='Pingid module for easy access to Pingid API'
+)
